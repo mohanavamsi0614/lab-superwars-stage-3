389050b8453650381d3a55afe343b2527a3209cc
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2023-11-07 21:25:33 +0530

    completed js with funcsions

--- src/app.js
+++ src/app.js
@@ -1,59 +1,70 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
-    return detailedPlayers;
-}
+  // Instead of forloop use Map method
+  // Code here
+  let x = players.map((v, i) => {
+    return {
+      name: v,
+      img: "images/super-" + (i + 1) + ".png",
+      str: getRandomStrength(),
+      type: i % 2 == 0 ? "hero" : "villain",
+    };
+  });
+  return x;
+};
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
+  return Math.ceil(Math.random() * 100) + 1;
+};
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
-
-    return fragment;
-}
+  // Instead of using for loop
+  // Use chaining of Array methods - filter, map and join
+  // Type your code here
+  f = players.filter((v) => {
+    return v.type ==type;
+  }).map((i)=>{
+    return "<div class='player'>"+
+    "<img src="+i.img+" alt='hdewio'>"+
+    "<div class='name'>"+i.name+"</div>"+
+    "<div class='strength'>"+i.str+"</div>"+
+ "</div>"   
+  });
+  return f
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+  document.getElementById("heroes").innerHTML += buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML += buildPlayers( players,"villain");
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
